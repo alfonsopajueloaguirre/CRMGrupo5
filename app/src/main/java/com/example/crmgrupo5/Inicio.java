e82f93ef687d1f33beff209aceaeddc68fd27f9d
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2019-11-28 18:34:05 +0100

    Inicio y Formato APP

--- app/src/main/java/com/example/crmgrupo5/Inicio.java
+++ app/src/main/java/com/example/crmgrupo5/Inicio.java
@@ -2,13 +2,23 @@ package com.example.crmgrupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
+    private Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        enter = findViewById(R.id.Enter);
+
+    }
+    public void Enter (View view){
+        Intent enterIntent = new Intent(Inicio.this, MainActivity.class);
+        startActivity(enterIntent);
     }
 }
