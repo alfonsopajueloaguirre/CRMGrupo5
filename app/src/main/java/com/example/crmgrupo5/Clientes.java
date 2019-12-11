d57638a3014b8acda51312a9880df1b80be85435
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2019-12-11 16:17:18 +0100

    BBDD RegistrarCliente

--- app/src/main/java/com/example/crmgrupo5/Clientes.java
+++ app/src/main/java/com/example/crmgrupo5/Clientes.java
@@ -22,9 +22,9 @@ public class Clientes extends AppCompatActivity {
     private AdminSQLiteOpenHelper BBDD;
     private String tipoBBDD = "BBDDCliente";
     private TextView textoContacto1,textoContacto2,textoContacto3,textoContacto4,textoContacto5,textoContacto6;
-    private TextView textoNombre1;
-    private TextView textoApellido1;
-    private TextView textoCorreo1;
+    private TextView textoNombre1,textoNombre2,textoNombre3,textoNombre4,textoNombre5,textoNombre6;
+    private TextView textoApellido1,textoApellido2,textoApellido3,textoApellido4,textoApellido5,textoApellido6;
+    private TextView textoCorreo1,textoCorreo2,textoCorreo3,textoCorreo4,textoCorreo5,textoCorreo6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +39,28 @@ public class Clientes extends AppCompatActivity {
         textoContacto3 = findViewById(R.id.textView_contacto3);
         textoContacto4 = findViewById(R.id.textView_contacto4);
         textoContacto5 = findViewById(R.id.textView_contacto5);
-        textoContacto6 = findViewById(R.id.textView_contacto5);
+        textoContacto6 = findViewById(R.id.textView_contacto6);
 
         textoNombre1 = findViewById(R.id.textView_nombre1);
+        textoNombre2 = findViewById(R.id.textView_nombre2);
+        textoNombre3 = findViewById(R.id.textView_nombre3);
+        textoNombre4 = findViewById(R.id.textView_nombre4);
+        textoNombre5 = findViewById(R.id.textView_nombre5);
+        textoNombre6 = findViewById(R.id.textView_nombre6);
 
-        //textoApellido1 = findViewById(R.id.te)
+        textoApellido1 = findViewById(R.id.textView_apellido1);
+        textoApellido2 = findViewById(R.id.textView_apellido2);
+        textoApellido3 = findViewById(R.id.textView_apellido3);
+        textoApellido4 = findViewById(R.id.textView_apellido4);
+        textoApellido5 = findViewById(R.id.textView_apellido5);
+        textoApellido6 = findViewById(R.id.textView_apellido6);
 
         textoCorreo1 = findViewById(R.id.textView_correo1);
+        textoCorreo2 = findViewById(R.id.textView_correo2);
+        textoCorreo3 = findViewById(R.id.textView_correo3);
+        textoCorreo4 = findViewById(R.id.textView_correo4);
+        textoCorreo5 = findViewById(R.id.textView_correo5);
+        textoCorreo6 = findViewById(R.id.textView_correo6);
 
 
 
@@ -65,10 +80,10 @@ public class Clientes extends AppCompatActivity {
     public void mostrarClientes(){
         BaseDeDatos = BBDD.getWritableDatabase();
         String columnas[] = new String[]{"nombre","apellido","correo"};
-        String j1="",j2="",j3="",j4="",j5="",j6="",p1="",p2="",p3="",p4="",p5="",p6="", c1,c2,c3,c4,c5,c6;
+        String j1="",j2="",j3="",j4="",j5="",j6="",p1="",p2="",p3="",p4="",p5="",p6="",c1="",c2="",c3="",c4="",c5="",c6="";
         String empty ="";
 
-        Cursor fila1 = BaseDeDatos.rawQuery("select * from "+tipoBBDD+"  order by nombreCliente DESC",null);
+        Cursor fila1 = BaseDeDatos.rawQuery("select * from "+tipoBBDD+"  order by nombreCliente ASC",null);
 
         if(fila1.moveToFirst()) {
             j1 = fila1.getString(0);
@@ -125,18 +140,25 @@ public class Clientes extends AppCompatActivity {
         }
 
         textoNombre1.setText(j1);
-        /*textoRanking2.setText(j2);
-        textoRanking3.setText(j3);
-        textoRanking4.setText(j4);
-        textoRanking5.setText(j5);
-        textoRanking6.setText(j6);*/
+        textoNombre2.setText(j2);
+        textoNombre3.setText(j3);
+        textoNombre4.setText(j4);
+        textoNombre5.setText(j5);
+        textoNombre6.setText(j6);
 
-        textoCorreo1.setText(p1);
-        /*mostrarPunt2.setText(p2);
-        mostrarPunt3.setText(p3);
-        mostrarPunt4.setText(p4);
-        mostrarPunt5.setText(p5);
-        mostrarPunt6.setText(p6);*/
+        textoApellido1.setText(p1);
+        textoApellido2.setText(p2);
+        textoApellido3.setText(p3);
+        textoApellido4.setText(p4);
+        textoApellido5.setText(p5);
+        textoApellido6.setText(p6);
+
+        textoCorreo1.setText(c1);
+        textoCorreo2.setText(c2);
+        textoCorreo3.setText(c3);
+        textoCorreo4.setText(c4);
+        textoCorreo5.setText(c5);
+        textoCorreo6.setText(c6);
 
         BaseDeDatos.close();
 
--- app/src/main/java/com/example/crmgrupo5/RegistrarCliente.java
+++ app/src/main/java/com/example/crmgrupo5/RegistrarCliente.java
@@ -3,6 +3,7 @@ package com.example.crmgrupo5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class RegistrarCliente extends AppCompatActivity {
 
     private SQLiteDatabase BasseDeDatos;
     private AdminSQLiteOpenHelper BBDD;
-    private EditText nombreC,apellidoC,correoC;
+    private TextInputEditText nombreC,apellidoC,correoC;
     private boolean registrado;
     private Button registrar;
 
@@ -71,6 +74,7 @@ public class RegistrarCliente extends AppCompatActivity {
             }
         }
         Toast.makeText(this, "Registrado", Toast.LENGTH_SHORT).show();
-
+        Intent intent1 = new Intent(RegistrarCliente.this, Clientes.class);
+        startActivity(intent1);
     }
 }
