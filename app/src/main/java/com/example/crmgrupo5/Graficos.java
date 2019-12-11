63cd4fb299019092bc20261f0eeda239eea28fff
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2019-12-11 18:05:03 +0100

    Spinner

--- app/src/main/java/com/example/crmgrupo5/Graficos.java
+++ app/src/main/java/com/example/crmgrupo5/Graficos.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Graficos extends AppCompatActivity {
 
     private ImageView grafico1,grafico2,grafico3;
+    private TextView textGanancias, textCostes;
+    private Spinner spinner;
     double random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graficos);
+
+        spinner = findViewById(R.id.spinner_operaciones);
+        textGanancias = findViewById(R.id.numGanancias);
+        textCostes = findViewById(R.id.numCostes);
+
+        String[] opciones = {"Televisión", "Radio", "Redes Sociales", "Anuncios web"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
+
+        spinner.setAdapter(adapter);
+
         grafico1 = findViewById(R.id.grafico1);
         grafico2 = findViewById(R.id.grafico2);
         grafico3 = findViewById(R.id.grafico1);
@@ -34,4 +49,26 @@ public class Graficos extends AppCompatActivity {
             grafico3.setVisibility(View.VISIBLE);
         }
     }
+
+    public void calcularPresupuesto(View view){
+        String seleccion = spinner.getSelectedItem().toString();
+        if (seleccion.equals("Televisión")) {
+            textCostes.setText("10.000");
+            textGanancias.setText("20.000");
+            // codigo set cuadrado a rojo ID (integer) IDColor (integer)
+        }
+        if (seleccion.equals("Radio")) {
+
+            // codigo set cuadrado a rojo ID (integer) IDColor (integer)
+        }
+        if (seleccion.equals("Redes Sociales")) {
+
+            // codigo set cuadrado a rojo ID (integer) IDColor (integer)
+        }
+        if (seleccion.equals("Anucios web")) {
+
+            // codigo set cuadrado a rojo ID (integer) IDColor (integer)
+        }
+    }
+
 }
