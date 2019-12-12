e9d19c8c9667cbb6d0bfb855df0167183479ea4d
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2019-12-12 11:19:04 +0100

    Update Reuniones.java

--- app/src/main/java/com/example/crmgrupo5/Reuniones.java
+++ app/src/main/java/com/example/crmgrupo5/Reuniones.java
@@ -1,6 +1,7 @@
 package com.example.crmgrupo5;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,9 +12,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 
 public class Reuniones extends AppCompatActivity {
-
+    private SQLiteDatabase BaseDeDatos;
+    private AdminSQLiteOpenHelper BBDD;
+    private String tipoBBDD = "BBDDReuniones";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        BBDD = new AdminSQLiteOpenHelper(this, "BBDDReuniones", null, 1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reuniones);
         Toolbar toolbar = findViewById(R.id.toolbar);
